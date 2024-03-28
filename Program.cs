2aafa11064e4ce001174e53394c999a808442b04
Maria <mariya-ivanova@list.ru>
2024-03-28 22:57:26 +0200

    the solution is ready to use

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿void CubicResult(int num){
+    Console.Write($"{num} -> ");
+    for (int i = 1; i < num; i++){
+        Console.Write(Math.Pow(i, 3) + ", ");
+    }
+    Console.WriteLine(Math.Pow(num, 3) + ".");
+}
+
+int GetNumber(string message){
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int digit = GetNumber("please, insert a number: ");
+CubicResult(digit);
